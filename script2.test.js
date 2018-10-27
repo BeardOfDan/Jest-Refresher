71417153e5d84a3500a054b4edbd9c65c8f4e4ca
Daniel Roberts <BeardOfDan@gmail.com>
2018-10-26 21:27:46 -0700

    (feat) Add additional test for getPeoplePromise

--- script2.test.js
+++ script2.test.js
@@ -9,6 +9,7 @@ describe('The swapi api', () => {
       const data = await swapi.getPeople(axios);
       expect(data.count).toBe(87);
     } catch (e) {
+      console.log(`Error: ${e}`);
       expect(true).toBe(false);
     } finally {
       done();
@@ -16,14 +17,16 @@ describe('The swapi api', () => {
   });
 
   it('calls swapi to get people with a promise', (done) => {
-    expect.assertions(1);
+    expect.assertions(2);
 
     swapi.getPeoplePromise(axios)
       .then((res) => {
         expect(res.count).toBe(87);
+        expect(res.results.length).toBeGreaterThan(5);
         done();
       })
       .catch((e) => {
+        console.log(`Error: ${e}`);
         expect(false).toBe(true);
         done();
       });
