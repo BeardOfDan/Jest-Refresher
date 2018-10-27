9ef63930e058fa31495f071f4311760b4cbc9b93
Daniel Roberts <BeardOfDan@gmail.com>
2018-10-26 21:14:57 -0700

    (fix) Protect against malformed url in getPeople
    Put api call and test inside of try/catch/finally

--- script2.test.js
+++ script2.test.js
@@ -5,11 +5,14 @@ describe('The swapi api', () => {
   it('calls swapi to get people', async (done) => {
     expect.assertions(1);
 
-    const data = await swapi.getPeople(axios);
-
-    expect(data.count).toBe(87);
-
-    done();
+    try { // protects against malformed url in swapi.getPeople
+      const data = await swapi.getPeople(axios);
+      expect(data.count).toBe(87);
+    } catch (e) {
+      expect(true).toBe(false);
+    } finally {
+      done();
+    }
   });
 
   it('calls swapi to get people with a promise', (done) => {
