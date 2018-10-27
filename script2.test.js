014612018546006b4c877d94f88be050eb2a0684
Daniel Roberts <BeardOfDan@gmail.com>
2018-10-26 23:57:25 -0700

    (feat) Use mockAxios for the getPeople test as well
    mockAxios was moved up to file scope, like axios and swapi, so it
    could be used by the getPeople test

--- script2.test.js
+++ script2.test.js
@@ -1,12 +1,17 @@
 const axios = require('axios');
 const swapi = require('./script2');
 
+const mockAxios = {};
+mockAxios.get = jest.fn().mockImplementation(() => {
+  return Promise.resolve({ data: { count: 87, results: new Array(87) } });
+});
+
 describe('getPeople', () => {
   it('calls swapi to get people', async (done) => {
     expect.assertions(1);
 
     try { // protects against malformed url in swapi.getPeople
-      const data = await swapi.getPeople(axios);
+      const data = await swapi.getPeople(mockAxios);
       expect(data.count).toBe(87);
     } catch (e) {
       console.log(`Error: ${e}`);
@@ -18,11 +23,6 @@ describe('getPeople', () => {
 });
 
 describe('getPeoplePromise', () => {
-  const mockAxios = {};
-  mockAxios.get = jest.fn().mockImplementation(() => {
-    return Promise.resolve({ data: { count: 87, results: new Array(87) } });
-  });
-
   it('calls swapi to get people with a promise', (done) => {
     expect.assertions(2);
 
@@ -44,7 +44,7 @@ describe('getPeoplePromise', () => {
 
     return swapi.getPeoplePromise(mockAxios)
       .then((res) => {
-        expect(mockAxios.get).toHaveBeenCalledTimes(2);
+        expect(mockAxios.get).toHaveBeenCalledTimes(3);
         expect(mockAxios.get).toHaveBeenCalledWith('https://swapi.co/api/people');
         expect(res.count).toBe(87);
         expect(res.results.length).toBeGreaterThan(10);
